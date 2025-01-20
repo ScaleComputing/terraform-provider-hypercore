39ca734e60fc60e085bbe16bf4e34479a666c43f
Justin Cinkelj <justin.cinkelj@xlab.si>
2025-01-20 17:23:15 +0100

    Pass 'make test'
    TF_ACC=1 make test
    TF_ACC=1 make testacc
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- internal/provider/scale_vm_resource_test.go
+++ internal/provider/scale_vm_resource_test.go
@@ -17,11 +17,20 @@ func TestAccScaleVMResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: testAccScaleVMResourceConfig("one"),
+				Config: testAccScaleVMResourceConfig("tf-vm", "tf-src"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scale_vm.test", "configurable_attribute", "one"),
-					resource.TestCheckResourceAttr("scale_vm.test", "defaulted", "example value when not configured"),
-					resource.TestCheckResourceAttr("scale_vm.test", "id", "scale-id"),
+					resource.TestCheckResourceAttr("scale_vm.test", "description", ""), // TODO "tf-vm-description"
+					resource.TestCheckResourceAttr("scale_vm.test", "memory", "0"),     // TODO 1 GB
+					// resource.TestCheckResourceAttr("scale_vm.test", "nics", []),
+					resource.TestCheckResourceAttr("scale_vm.test", "network_mode", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "group", ""), // TODO "testtf"
+					resource.TestCheckResourceAttr("scale_vm.test", "meta_data", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "user_data", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "network_iface", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "source_vm_name", "tf-src"),
+					resource.TestCheckResourceAttr("scale_vm.test", "name", "tf-vm"),
+					resource.TestCheckResourceAttr("scale_vm.test", "vcpu", "0"),
+					resource.TestCheckResourceAttr("scale_vm.test", "disk_size", "0"),
 				),
 			},
 			// ImportState testing
@@ -33,13 +42,31 @@ func TestAccScaleVMResource(t *testing.T) {
 				// example code does not have an actual upstream service.
 				// Once the Read method is able to refresh information from
 				// the upstream service, this can be removed.
-				ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
+				ImportStateVerifyIgnore: []string{
+					"vm_list",
+					// TODO do not ignore below attributes
+					"name",
+					"description",
+					"group",
+					"vcpu",
+					"memory",
+					"disk_size",
+					"network_iface",
+					"network_mode",
+					"source_vm_name",
+					"user_data",
+					"meta_data",
+				},
 			},
 			// Update and Read testing
 			{
-				Config: testAccScaleVMResourceConfig("two"),
+				Config: testAccScaleVMResourceConfig("tf-vm", "tf-src"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scale_vm.test", "configurable_attribute", "two"),
+					resource.TestCheckResourceAttr("scale_vm.test", "name", "tf-vm"),
+					resource.TestCheckResourceAttr("scale_vm.test", "description", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "group", ""),
+					resource.TestCheckResourceAttr("scale_vm.test", "vcpu", "0"),
+					resource.TestCheckResourceAttr("scale_vm.test", "memory", "0"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
@@ -47,10 +74,21 @@ func TestAccScaleVMResource(t *testing.T) {
 	})
 }
 
-func testAccScaleVMResourceConfig(configurableAttribute string) string {
+func testAccScaleVMResourceConfig(vm_name string, source_vm_name string) string {
 	return fmt.Sprintf(`
 resource "scale_vm" "test" {
-  configurable_attribute = %[1]q
+  name = %[1]q
+  source_vm_name = %[2]q
+  group = ""
+  vcpu = 0
+  memory = 0
+  network_mode = ""
+  user_data = ""
+  meta_data = ""
+  network_iface = ""
+  description = ""
+  nics = []
+  disk_size = 0
 }
-`, configurableAttribute)
+`, vm_name, source_vm_name)
 }
