2d35b3a04738aef35b4b3cf0ec25e859ac41ae3f
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2025-03-27 13:30:28 +0000

    added test template for ISO

---
+++ internal/provider/tests/acceptance/hypercore_iso_resource_acc_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccHypercoreISOResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccHypercoreISOResourceConfig("testtf-iso.iso"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("hypercore_iso.test", "name", "testtf-iso.iso"),
+				),
+			},
+		},
+	})
+}
+
+func testAccHypercoreISOResourceConfig(iso_name string) string {
+	return fmt.Sprintf(`
+resource "hypercore_iso" "test" {
+  name = "testtf-iso.iso"
+  source_url = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/aarch64/alpine-virt-3.21.3-aarch64.iso"
+}
+`)
+}
