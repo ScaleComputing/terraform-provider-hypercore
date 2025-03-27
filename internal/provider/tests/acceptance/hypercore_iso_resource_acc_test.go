f7fd378cc24e606f21cfbe543a9c4480a99dce3d
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2025-03-27 13:34:24 +0000

    Linter errors fix

--- internal/provider/tests/acceptance/hypercore_iso_resource_acc_test.go
+++ internal/provider/tests/acceptance/hypercore_iso_resource_acc_test.go
@@ -29,8 +29,8 @@ func TestAccHypercoreISOResource(t *testing.T) {
 func testAccHypercoreISOResourceConfig(iso_name string) string {
 	return fmt.Sprintf(`
 resource "hypercore_iso" "test" {
-  name = "testtf-iso.iso"
+  name       = "%s"
   source_url = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/aarch64/alpine-virt-3.21.3-aarch64.iso"
 }
-`)
+`, iso_name)
 }
