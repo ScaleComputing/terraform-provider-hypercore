d958688eef31253c4b3aa06d5af9e91ea6a49d68
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2025-04-03 11:57:31 +0000

    nic test template

--- internal/provider/tests/acceptance/hypercore_nic_resource_acc_test.go
+++ internal/provider/tests/acceptance/hypercore_nic_resource_acc_test.go
@@ -33,13 +33,6 @@ func TestAccHypercoreNicResource(t *testing.T) {
 					resource.TestCheckResourceAttr("hypercore_nic.test", "type", "VIRTIO"),
 				),
 			},
-			{
-				Config: testAccHypercoreNicResourceConfig(),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("hypercore_nic.testa", "vlan", "12"),
-					resource.TestCheckResourceAttr("hypercore_nic.testa", "type", "VIRTIO"),
-				),
-			},
 		},
 	})
 }
@@ -72,14 +65,4 @@ output "vm_id" {
   value = data.hypercore_vm.nicvm.vms.0.uuid
 }
 `, source_vm_uuid, test_vm_name)
-}
-
-func testAccHypercoreNicResourceConfig() string {
-	return `
-resource "hypercore_nic" "testa" {
-  vm_uuid = "${output.vm_id}"
-  vlan    = 12
-  type    = "VIRTIO"
-}
-`
 }
