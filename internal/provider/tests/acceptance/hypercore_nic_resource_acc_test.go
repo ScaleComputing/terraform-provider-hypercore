9b48e057e4f9537b3deee831868a5bc9e2070c91
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2025-04-03 10:49:52 +0000

    nic test template

--- internal/provider/tests/acceptance/hypercore_nic_resource_acc_test.go
+++ internal/provider/tests/acceptance/hypercore_nic_resource_acc_test.go
@@ -8,12 +8,10 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 var source_vm_uuid = "97904009-1878-4881-b6df-83c85ab7dc1a"
 var test_vm_name = "integration-test-vm-nic"
-var test_vm_uuid = ""
 
 //var source_vm_name = "integration-test-vm"
 
@@ -26,16 +24,6 @@ func TestAccHypercoreNicResource(t *testing.T) {
 			{
 				Config: testAccHypercoreSourceVMRConfig(source_vm_uuid),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					// Store cloned VM uuid for later use
-					func(s *terraform.State) error {
-						rs, ok := s.RootModule().Resources["hypercore_vm.test"]
-						if !ok {
-							return fmt.Errorf("not found: %s", test_vm_name)
-						}
-						test_vm_uuid = rs.Primary.Attributes["id"]
-						fmt.Printf("Captured Resource ID: %s\n", test_vm_uuid)
-						return nil
-					},
 					resource.TestCheckResourceAttr("hypercore_vm.test", "name", test_vm_name),
 					resource.TestCheckResourceAttr("hypercore_vm.test", "description", "integration-vm-description"),
 					resource.TestCheckResourceAttr("hypercore_vm.test", "group", "Xlabintegrationtest"),
@@ -45,12 +33,7 @@ func TestAccHypercoreNicResource(t *testing.T) {
 			},
 			// Create new NIC
 			{
-				Config: func() string {
-					if test_vm_uuid == "" {
-						panic("test_vm_uuid is empty! Ensure VM is created successfully before using it.")
-					}
-					return testAccHypercoreNicResourceConfig(test_vm_uuid)
-				}(),
+				Config: testAccHypercoreNicResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("hypercore_nic.test", "vlan", "11"),
 					resource.TestCheckResourceAttr("hypercore_nic.test", "type", "VIRTIO"),
@@ -77,12 +60,12 @@ resource "hypercore_vm" "test" {
 `, source_vm_uuid)
 }
 
-func testAccHypercoreNicResourceConfig(test_vm_uuid string) string {
-	return fmt.Sprintf(`
+func testAccHypercoreNicResourceConfig() string {
+	return `
 resource "hypercore_nic" "test" {
-  vm_uuid = %[1]q
+  vm_uuid = hypercore_vm.test.id
   vlan    = 11
   type    = "VIRTIO"
 }
-`, test_vm_uuid)
+`
 }
