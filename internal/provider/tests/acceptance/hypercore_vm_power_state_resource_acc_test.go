d190259fe3356d9858c2cfee77256db3424aa30e
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2025-04-07 11:19:47 +0000

    power state resource test

--- internal/provider/tests/acceptance/hypercore_vm_power_state_resource_acc_test.go
+++ internal/provider/tests/acceptance/hypercore_vm_power_state_resource_acc_test.go
@@ -19,7 +19,6 @@ func TestAccHypercorePowerStateResource(t *testing.T) {
 				Config: testAccHypercorePowerStateResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("hypercore_vm_power_state.power_state_test", "state", "RUNNING"),
-					resource.TestCheckResourceAttr("hypercore_vm_power_state.power_state_test_cleanup", "state", "SHUTOFF"),
 				),
 			},
 		},
@@ -28,15 +27,6 @@ func TestAccHypercorePowerStateResource(t *testing.T) {
 
 func testAccHypercorePowerStateResourceConfig() string {
 	return fmt.Sprintf(`
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0.0"
-    }
-  }
-}
-
 data "hypercore_vm" "integrationvm" {
   name = %[1]q
 }
@@ -45,23 +35,5 @@ resource "hypercore_vm_power_state" "power_state_test" {
   vm_uuid = data.hypercore_vm.integrationvm.vms.0.uuid
   state   = "RUNNING"
 }
-
-resource "null_resource" "wait_before" {
-  provisioner "local-exec" {
-    command = "sleep 15"
-  }
-}
-
-resource "hypercore_vm_power_state" "power_state_test_cleanup" {
-  vm_uuid = data.hypercore_vm.integrationvm.vms.0.uuid
-  state   = "SHUTOFF"
-}
-
-resource "null_resource" "wait_after" {
-  provisioner "local-exec" {
-    command = "sleep 15"
-  }
-}
-
 `, source_vm_name)
 }
