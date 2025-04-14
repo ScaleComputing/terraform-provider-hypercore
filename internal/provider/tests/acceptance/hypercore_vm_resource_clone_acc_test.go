9c4d9bed352e1d7fdac15f52cf5dcd94ae132a5c
Domen <domen.dobnikar@xlab.si>
2025-04-14 14:09:46 +0200

    VM import integration test

--- internal/provider/tests/acceptance/hypercore_vm_resource_clone_acc_test.go
+++ internal/provider/tests/acceptance/hypercore_vm_resource_clone_acc_test.go
@@ -10,19 +10,18 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-var requested_power_state string = "stop" // "started"
 // UUID of VM with name "testtf_src"
 // var testtf_src_uuid string = "27af8248-88ee-4420-85d7-78b735415064"  // https://172.31.6.11
 var testtf_src_uuid string = "ff36479e-06bb-4141-bad5-0097c8c1a4a6" // https://10.5.11.205
 
-func TestAccHypercoreVMResource(t *testing.T) {
+func TestAccHypercoreVMResourceClone(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: testAccHypercoreVMResourceConfig("testtf-vm", testtf_src_uuid),
+				Config: testAccHypercoreVMResourceCloneConfig("testtf-vm", testtf_src_uuid),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("hypercore_vm.test", "description", "testtf-vm-description"),
 					resource.TestCheckResourceAttr("hypercore_vm.test", "memory", "4096"),
@@ -64,7 +63,7 @@ func TestAccHypercoreVMResource(t *testing.T) {
 			*/
 			// Update and Read testing
 			{
-				Config: testAccHypercoreVMResourceConfig("testtf-vm", testtf_src_uuid),
+				Config: testAccHypercoreVMResourceCloneConfig("testtf-vm", testtf_src_uuid),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("hypercore_vm.test", "name", "testtf-vm"),
 					resource.TestCheckResourceAttr("hypercore_vm.test", "description", "testtf-vm-description"),
@@ -79,7 +78,7 @@ func TestAccHypercoreVMResource(t *testing.T) {
 	})
 }
 
-func testAccHypercoreVMResourceConfig(vm_name string, source_vm_uuid string) string {
+func testAccHypercoreVMResourceCloneConfig(vm_name string, source_vm_uuid string) string {
 	return fmt.Sprintf(`
 resource "hypercore_vm" "test" {
   name = %[1]q
